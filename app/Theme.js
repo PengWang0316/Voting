3316945a05137021ea772e831d1f212e54b5f5a2
Kevin Wang <rocwang0316@gmail.com>
2018-10-27 15:35:13 -0700

    Added the Theme file

---
+++ app/Theme.js
@@ -0,0 +1,16 @@
+export default {
+  primary: {
+    light: '#b2fab4',
+    main: '#81c784',
+    dark: '#519657',
+    contrastText: '#fff',
+  },
+  secondary: {
+    light: '#4fb3bf',
+    main: '#00838f',
+    dark: '#005662',
+    contrastText: '#fff',
+  },
+  panelBackgroundColor: '#F5FCFF',
+  dangerousColor: '#ff1744',
+};
