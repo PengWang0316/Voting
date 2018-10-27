26ed238db77ce9f341caa929e6c9e741718385b4
Kevin Wang <rocwang0316@gmail.com>
2018-10-27 16:37:37 -0700

    Added the UserActions and test.

---
+++ __tests__/actions/UserActions.js
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { USER_LOGOUT_SUCCESS, USER_LOGIN_SUCCESS, PARSER_USER_FROM_JWT_SUCCESS } from '../../app/actions/ActionTypes';
+import {
+  API_JWTMESSAGE_VERIFY, API_LOGIN_WITH_PASSWORD,
+} from '../../app/actions/ApiUrls';
+// import { JWT_MESSAGE } from '../../app/config';
+import * as UserActions from '../../app/actions/UserActions';
+
+const axiosMock = new MockAdapter(axios); // Setting up a mock for axios.
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('UserActions', () => {
+  test('logout', () => {
+    // localStorage.setItem(JWT_MESSAGE, 'message');
+    const expectActions = [{ type: USER_LOGOUT_SUCCESS, user: {} }];
+    const store = mockStore();
+    store.dispatch(UserActions.logout());
+    expect(store.getActions()).toEqual(expectActions);
+    // expect(localStorage.removeItem).toHaveBeenCalledTimes(1);
+    // expect(localStorage.removeItem).toHaveBeenLastCalledWith(JWT_MESSAGE);
+    // expect(localStorage.getItem(JWT_MESSAGE)).toBeNull();
+  });
+
+  test('parserUserFromJwt', () => {
+    const user = { _id: 'id' };
+    const jwtMessage = 'message';
+    const expectActions = [
+      { type: PARSER_USER_FROM_JWT_SUCCESS, user },
+    ];
+    axiosMock.onGet(API_JWTMESSAGE_VERIFY, { params: { jwtMessage } }).reply(200, user);
+    const store = mockStore();
+    return store.dispatch(UserActions.parserUserFromJwt(jwtMessage)).then(() => expect(store.getActions()).toEqual(expectActions));
+  });
+
+  test('parserUserFromJwt with network error', () => {
+    const jwtMessage = 'message';
+    const mockErrorFn = jest.fn();
+    console.error = mockErrorFn;
+
+    axiosMock.onGet(API_JWTMESSAGE_VERIFY, { params: { jwtMessage } }).networkError();
+    const store = mockStore();
+    store.dispatch(UserActions.parserUserFromJwt(jwtMessage)).then(() => expect(mockErrorFn).toHaveBeenCalledTimes(1));
+  });
+
+  test('loginWithPassword has data without error', () => {
+    // localStorage.removeItem(JWT_MESSAGE);  React Native could not use the localStorage
+    const user = { username: 'username', password: 'password' };
+    const expectActions = [
+      { type: USER_LOGIN_SUCCESS, user: { ...user, jwt: 'jwt' } }
+    ];
+    axiosMock.onGet(API_LOGIN_WITH_PASSWORD, { params: user }).reply(200, { ...user, jwt: 'jwt' });
+    const store = mockStore();
+    return store.dispatch(UserActions.loginWithPassword(user)).then(() => {
+      // expect(localStorage.getItem(JWT_MESSAGE)).toBe('jwt');
+      expect(store.getActions()).toEqual(expectActions);
+    });
+  });
+
+  test('loginWithPassword no data without error', () => {
+    // localStorage.removeItem(JWT_MESSAGE); React Native could not use the localStorage
+    const user = { username: 'username', password: 'password' };
+    const expectActions = [
+      { type: USER_LOGIN_SUCCESS, user: null },
+    ];
+    axiosMock.onGet(API_LOGIN_WITH_PASSWORD, { params: user }).reply(200, null);
+    const store = mockStore();
+    return store.dispatch(UserActions.loginWithPassword(user)).then(() => {
+      // expect(localStorage.getItem(JWT_MESSAGE)).toBeNull();
+      expect(store.getActions()).toEqual(expectActions);
+    });
+  });
+
+  test('loginWithPassword with network error', () => {
+    const user = { username: 'username', password: 'password' };
+    const mockErrorFn = jest.fn();
+    console.error = mockErrorFn;
+    axiosMock.onGet(API_LOGIN_WITH_PASSWORD, { params: user }).networkError();
+    const store = mockStore();
+    return store.dispatch(UserActions.loginWithPassword(user))
+      .then(() => expect(mockErrorFn).toHaveBeenCalledTimes(1));
+  });
+
+  test('emptyUser', () => {
+    const expectActions = [
+      { type: USER_LOGIN_SUCCESS, user: {} },
+    ];
+    const store = mockStore();
+    store.dispatch(UserActions.emptyUser());
+    expect(store.getActions()).toEqual(expectActions);
+  });
+});
---
+++ app/actions/ApiUrls.js
@@ -0,0 +1,22 @@
+import { BASE_URL } from '../config';
+
+// BASE_URL = ""; //Production server
+export const API_BASE_URL = `${BASE_URL}/api/v1`;
+// export const API_FACEBOOK_LOGIN = `${API_BASE_URL}/auth/facebookLogin`;
+// export const API_GOOGLE_LOGIN = `${API_BASE_URL}/auth/googleLogin`;
+export const API_JWTMESSAGE_VERIFY = `${API_BASE_URL}/jwtMessageVerify`;
+export const API_USERNAME_PASSWORD_LOGIN = `${API_BASE_URL}/auth/usernamePasswordLogin`;
+// export const API_CHECK_USERNAME_AVAILABLE = `${API_BASE_URL}/auth/checkUsernameAvailable`;
+
+// export const API_FETCH_BASIC_INFORMATION = `${API_BASE_URL}/fetchBasicInformation`;
+// export const API_FETCH_ALL_MENU = `${API_BASE_URL}/fetchAllMenu`;
+// export const API_REGISTER_USER = `${API_BASE_URL}/auth/registerUser`;
+export const API_LOGIN_WITH_PASSWORD = `${API_BASE_URL}/auth/loginWithPassword`;
+// export const API_ADD_ITEM_TO_ORDER = `${API_BASE_URL}/addItemToOrder`;
+// export const API_SAVE_PLACED_ORDER = `${API_BASE_URL}/savePlacedOrder`;
+// export const API_FETCH_ORDER_AMOUNT = `${API_BASE_URL}/fetchOrderAmount`;
+// export const API_FETCH_ORDERS = `${API_BASE_URL}/fetchOrders`;
+// export const API_FETCH_UNLOGIN_ORDERS = `${API_BASE_URL}/fetchUnloginOrders`;
+// export const API_LINK_ORDER_TO_ACCOUNT = `${API_BASE_URL}/linkOrderToAccount`;
+// export const API_FETCH_UNFINISHED_ORDERS = `${API_BASE_URL}/fetchUnfinishedOrders`;
+// export const API_UPDATE_FINISHED_ITEMS = `${API_BASE_URL}/updateFinishedItems`;
