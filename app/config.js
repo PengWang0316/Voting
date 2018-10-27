26d3876ca3361302d5850061e61f7e9010680884
Kevin Wang <rocwang0316@gmail.com>
2018-10-27 16:37:57 -0700

    Added the config.js file.

---
+++ app/config.js
@@ -0,0 +1,13 @@
+export const JWT_MESSAGE = 'Voting_JWT';
+// export const BASE_URL = 'https://orders-taker.glitch.me'; // Development server
+// export const BASE_URL = 'https://orderstaker.kevin-project.com:8081'; // Development server on the AWS
+export const BASE_URL = 'https://orderstaker.kevin-project.com:8080'; // Production server
+// export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3001';
+export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3000'; // Production server
+
+/* URLS */
+// export const HOME_PAGE_URL = '/';
+// export const LOGIN_REDIRECT_RUL = '/loginRedirect';
+// export const CART_PAGE_URL = '/cartPage';
+// export const ORDERS_PAGE_URL = '/ordersPage';
+// export const KITHEN_INTERFACE_PAGE_URL = '/kithenInterfacePage';
