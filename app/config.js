537b16489b16102c36a52d2679c967596e77981b
Kevin Wang <rocwang0316@gmail.com>
2018-10-30 15:17:11 -0700

    Changed the base api url to the cloud.

--- app/config.js
+++ app/config.js
@@ -1,9 +1,9 @@
 export const JWT_MESSAGE = 'Voting_JWT';
 // export const BASE_URL = 'https://orders-taker.glitch.me'; // Development server
 // export const BASE_URL = 'https://orderstaker.kevin-project.com:8081'; // Development server on the AWS
-export const BASE_URL = 'https://orderstaker.kevin-project.com:8080'; // Production server
+export const BASE_URL = 'https://orderstaker.kevin-project.com:8081'; // Production server
 // export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3001';
-export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3000'; // Production server
+// export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3000'; // Production server
 
 /* URLS */
 // export const HOME_PAGE_URL = '/';
