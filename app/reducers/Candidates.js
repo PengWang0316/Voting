db6851cba926cd89208306fb05abf4505b3dbc24
Kevin Wang <rocwang0316@gmail.com>
2018-11-03 15:23:43 -0700

    Update the Candidates Reducer to handle ADD_VOTE_SUCCESS

--- app/reducers/Candidates.js
+++ app/reducers/Candidates.js
@@ -1,9 +1,16 @@
-import { FETCH_CANDIDATES_INFO_SUCCESS } from '../actions/ActionTypes';
+import { FETCH_CANDIDATES_INFO_SUCCESS, ADD_VOTE_SUCCESS } from '../actions/ActionTypes';
 
 const candidates = (state = null, action) => {
   switch (action.type) {
     case FETCH_CANDIDATES_INFO_SUCCESS:
       return action.candidates;
+    case ADD_VOTE_SUCCESS: { // In this case we know we just have two candidates. So, iterating all element is not a big problem.
+      const newCandidates = [];
+      state.forEach(candidate => {
+        newCandidates.push({ ...candidate, votes: candidate.id === action.candidateId ? candidate.vote + 1 : candidate.vote });
+      });
+      return newCandidates;
+    }
     default:
       return state;
   }
