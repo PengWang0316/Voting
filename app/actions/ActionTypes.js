bc6ae26785f773f374de961e03c2d09f993dde87
Kevin Wang <rocwang0316@gmail.com>
2018-11-03 14:41:55 -0700

    Added more action types for the User and Candidate action.

--- app/actions/ActionTypes.js
+++ app/actions/ActionTypes.js
@@ -2,3 +2,5 @@ export const USER_LOGOUT_SUCCESS = 'userLogoutSuccess';
 export const USER_LOGIN_SUCCESS = 'userLoginSuccess';
 export const PARSER_USER_FROM_JWT_SUCCESS = 'parserUserFromJwtSuccess';
 export const FETCH_CANDIDATES_INFO_SUCCESS = 'fetchCandidateSInfoSuccess';
+export const VOTE_SUCCESS = 'voteSuccess';
+export const ADD_VOTE_SUCCESS = 'ADD_VOTE_SUCCESS';
