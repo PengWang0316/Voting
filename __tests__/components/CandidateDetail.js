46fc9855dad0be13ecd6e32fa737fbed72e99f8d
Kevin Wang <rocwang0316@gmail.com>
2018-11-03 14:41:22 -0700

    Added vote call for the component.

--- __tests__/components/CandidateDetail.js
+++ __tests__/components/CandidateDetail.js
@@ -13,10 +13,32 @@ describe('CandidateDetail', () => {
       photo: 'photoURI',
       name: 'name',
       description: 'description',
+      id: 'candidateId',
     },
     user: {},
+    vote: jest.fn(),
   };
+  const getShallowComponent = (props = defaultProps) => shallow(<CandidateDetail {...props} />);
+
+  test('Initial states', () => {
+    const component = getShallowComponent();
+    expect(component.state('isSubmited')).toBe(false);
+  });
+
+  test('handleVoteBtnClick', () => {
+    const component = getShallowComponent({ ...defaultProps, user: { id: 'userId' } });
+    component.instance().handleVoteBtnClick();
+    expect(component.state('isSubmited')).toBe(true);
+    expect(defaultProps.vote).toHaveBeenCalledTimes(1);
+    expect(defaultProps.vote).toHaveBeenLastCalledWith('userId', 'candidateId');
+  });
 
   test('Snapshot without user id', () => expect(renderer.create(<CandidateDetail {...defaultProps} />).toJSON()).toMatchSnapshot());
   test('Snapshot with user id', () => expect(renderer.create(<CandidateDetail {...{ ...defaultProps, user: { id: 'id' } }} />).toJSON()).toMatchSnapshot());
+  test('Snapshot with user id and vote_id', () => expect(renderer.create(<CandidateDetail {...{ ...defaultProps, user: { id: 'id', vote_id: 'voteId' } }} />).toJSON()).toMatchSnapshot());
+  test('Snapshot with user id, vote_id and isSubmited is true', () => {
+    const component = getShallowComponent({ ...defaultProps, user: { id: 'id', vote_id: 'voteId' } });
+    component.setState({ isSubmited: true });
+    expect(renderer.create(component).toJSON()).toMatchSnapshot();
+  });
 });
--- app/components/CandidateDetail/CandidateDetail.js
+++ app/components/CandidateDetail/CandidateDetail.js
@@ -1,44 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import CacheableImage from 'react-native-cacheable-image';
 import { Button, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 
 import Styles from './Styles';
+import { vote } from '../../actions/UserActions';
 
 type Props = {
   candidate: Object,
   user: Object,
+  vote: Function,
 };
 
-export const CandidateDetail = ({ candidate, user }: Props) => (
-  <View style={[Styles.root, candidate.isDemocracy ? Styles.democracyBg : Styles.republicBg]}>
-    <ScrollView>
-      <View style={Styles.interView}>
-        <CacheableImage
-          style={Styles.avatar}
-          source={{ uri: candidate.photo }}
-        />
-        <Text style={Styles.nameText}>{candidate.name}</Text>
-        <Text style={Styles.descriptionText}>{candidate.description}</Text>
-        <Button
-          icon={(
-            <Icon
-              name="vote"
-              type="material-community"
-              color="white"
+/** The component to show the detailed information for a candidate. */
+export class CandidateDetail extends Component<Props> {
+  state = { isSubmited: false };
+
+  /**
+   * Calling the vote action and change the isSubmited state to true.
+   * @return {null} No return.
+   */
+  handleVoteBtnClick = () => this.setState(
+    { isSubmited: true },
+    () => this.props.vote(this.props.user.id, this.props.candidate.id),
+  );
+
+  /**
+   * The render method
+   * @return {jsx} Return jsx for the component.
+   */
+  render() {
+    const { candidate, user } = this.props;
+    const { isSubmited } = this.state;
+    return (
+      <View style={[Styles.root, candidate.isDemocracy ? Styles.democracyBg : Styles.republicBg]}>
+        <ScrollView>
+          <View style={Styles.interView}>
+            <CacheableImage
+              style={Styles.avatar}
+              source={{ uri: candidate.photo }}
             />
-          )}
-          title="VOTE FOR ME"
-          buttonStyle={Styles.voteBtn}
-          disabled={!user.id}
-        />
+            <Text style={Styles.nameText}>{candidate.name}</Text>
+            <Text style={Styles.descriptionText}>{candidate.description}</Text>
+            <Button
+              icon={(
+                <Icon
+                  name="vote"
+                  type="material-community"
+                  color="white"
+                />
+              )}
+              title="VOTE FOR ME"
+              buttonStyle={Styles.voteBtn}
+              disabled={!user.id || !!(user.vote_id) || isSubmited}
+              onPress={this.handleVoteBtnClick}
+            />
+          </View>
+        </ScrollView>
       </View>
-    </ScrollView>
-  </View>
-);
+    );
+  }
+}
+
 /* istanbul ignore next */
 const mapStateToProps = state => ({
   user: state.user,
 });
-export default connect(mapStateToProps, null)(CandidateDetail);
+/* istanbul ignore next */
+const mapDispatchToProps = dispatch => ({
+  vote: (userId, candidateId) => dispatch(vote(userId, candidateId)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CandidateDetail);
