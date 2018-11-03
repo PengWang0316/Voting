a245a0145f17e519a0c7338030d370c00dea5109
Kevin Wang <rocwang0316@gmail.com>
2018-11-03 15:24:14 -0700

    Updated the UserReducers to handle VOTE_SUCCESS.

--- app/reducers/UserReducers.js
+++ app/reducers/UserReducers.js
@@ -1,4 +1,7 @@
-import { USER_LOGOUT_SUCCESS, USER_LOGIN_SUCCESS, PARSER_USER_FROM_JWT_SUCCESS } from '../actions/ActionTypes';
+import {
+  USER_LOGOUT_SUCCESS, USER_LOGIN_SUCCESS,
+  PARSER_USER_FROM_JWT_SUCCESS, VOTE_SUCCESS,
+} from '../actions/ActionTypes';
 
 const user = (state = {}, action) => {
   switch (action.type) {
@@ -6,6 +9,8 @@ const user = (state = {}, action) => {
     case USER_LOGIN_SUCCESS:
     case PARSER_USER_FROM_JWT_SUCCESS:
       return action.user;
+    case VOTE_SUCCESS:
+      return { ...state, vote_id: action.vote_id };
     default:
       return state;
   }
